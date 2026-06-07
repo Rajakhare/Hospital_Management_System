9fc6f2287ce2440d189882b224fd6cda38d84b54
@Raj_Akhare <157144409+Rajakhare@users.noreply.github.com>
2026-06-07 22:24:46 +0530

    Fixed Admin Register Css

--- frontend/src/Components/Admin/AdminReg.jsx
+++ frontend/src/Components/Admin/AdminReg.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
+import "../Patient/patient.css"
 
 let AdminReg = () => {
     let navigate = useNavigate()
